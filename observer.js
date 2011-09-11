a3d38a624b07096f1dcf80b2d5931ae25d42af81
Aurojit Panda <apanda@Hausdorff.local>
2011-09-11 15:53:44 -0700

    Working save/conversion from base 64 to binary data

--- observer.js
+++ observer.js
@@ -8,7 +8,7 @@
     redisClient.on("pmessage", function(pattern, channel, message) {
         var obj = JSON.parse(message);
         var str = obj["data"];
-        var data = str.split(',', 1)[1];
+        var data = str.split(',', 2)[1];
         var buffer = new Buffer(data, 'base64');
         var stream = fs.createWriteStream(channel+".jpg");
         stream.once('open', function(fd) {
