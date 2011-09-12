2e4a4284ea74bcf626f95259d80bebe0b4462a64
Aurojit Panda <apanda@Hausdorff.local>
2011-09-11 19:34:56 -0700

    Save to S3

--- observer.js
+++ observer.js
@@ -3,14 +3,31 @@
     var redisClient;
     var fs = require('fs');
     redis = require('redis');
+    var knox = require('knox');
     redisClient = redis.createClient();
-
+    var knoxClient = knox.createClient({key:'AKIAIOPRNXFE5YO3VOWQ', 
+                       secret:'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',
+                       bucket:'apanda'});
     redisClient.on("pmessage", function(pattern, channel, message) {
         var obj = JSON.parse(message);
         var str = obj["data"];
         var data = str.split(',', 2)[1];
         var buffer = new Buffer(data, 'base64');
         var stream = fs.createWriteStream(channel+".jpg");
+	var contentLength = buffer.length;
+        var req = knoxClient.put('/images/'+channel+'.jpg', {
+                 'Content-Length': buffer.length,
+                 'Content-Type':obj["type"]
+        });
+        req.on('response', function(res){
+                   if (200 == res.statusCode) {
+                        console.log('saved to %s', req.url);
+                   }
+                   else {
+                       console.log('error %d', req.statusCode);
+                   }
+              });
+        req.end(buffer);
         stream.once('open', function(fd) {
             stream.write(buffer);
         });
